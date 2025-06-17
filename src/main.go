c8924024ba322f827db4c4de2e02d41891cca9f8
Victor Ramos <v.ramos58@hotmail.com>
2025-06-17 07:01:33 -0300

    feat: configs

--- src/main.go
+++ src/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/victorramos887/go_pdf/src/router"
+import (
+	"fmt"
+
+	"github.com/victorramos887/go_pdf/src/config"
+	"github.com/victorramos887/go_pdf/src/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+
+	logger = *config.GetLogger("main")
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		fmt.Println("error", err)
+		return
+	}
 	router.Initialize()
 }
